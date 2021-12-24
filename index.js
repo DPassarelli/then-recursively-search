33c4d24f08cb4b92d710068618840eb526498f92
David Passarelli <dpassarelli@camelotcg.com>
2021-12-24 11:16:27 -0500

    Decided the functions weren't numerous enough to require putting them into a separate folder. Too much overhead?

--- index.js
+++ index.js
@@ -1,8 +1,59 @@
 const callsites = require('callsites')
 const debug = require('debug')('then-recursively-search')
+const fs = require('fs').promises
 const path = require('path')
 
-const recursivelySearchForFile = require(path.join(__dirname, './lib/recursively-search.js'))
+/**
+ * Returns a Promise that resolves to a Boolean value indicating whether the
+ * named file exists in the specified folder.
+ *
+ * @param  {String}    filename   The named file (including extension).
+ *
+ * @param  {String}    dirname    The absolute path of the containing folder.
+ *
+ * @return {Promise}              Resolves to a {Boolean}.
+ */
+async function doesFileExist (filename, dirname) {
+  // fs.exists() is deprecated; docs recommend using access() instead of stat()
+  try {
+    await fs.access(path.join(dirname, filename))
+    return true
+  } catch {
+    return false
+  }
+}
+
+/**
+ * Returns the parent of the specified folder.
+ *
+ * @param  {String}   dirname   The absolute path of the child folder.
+ *
+ * @return {String}             The absolute path to the parent.
+ */
+function getParentFolder (dirname) {
+  return path.dirname(dirname)
+}
+
+/**
+ * Search for `filename` in `dirname`, recursively moving up the directory tree
+ * if not found.
+ *
+ * @param  {String}    filename   The file to find (including extension).
+ *
+ * @param  {String}    dirname    The absolute path of the folder to look in.
+ *
+ * @return {Promise}              Resolves to a {String} containing the complete
+ *                                path to the file.
+ */
+async function searchForFile (filename, dirname) {
+  const found = await doesFileExist(filename, dirname)
+
+  if (found) {
+    return path.join(dirname, filename)
+  }
+
+  return await searchForFile(filename, getParentFolder(dirname))
+}
 
 async function exported (filename, startIn) {
   if (startIn === undefined) {
@@ -12,7 +63,7 @@ async function exported (filename, startIn) {
 
   debug('searching for "%s" in "%s"', filename, startIn)
 
-  return recursivelySearchForFile(filename, startIn)
+  return searchForFile(filename, startIn)
 }
 
 module.exports = exported
